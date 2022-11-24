1eb50a0017f37b1cc6d97915e778e497b58c37f1
jaydlove <51937167+jaydlove@users.noreply.github.com>
2022-11-24 00:06:31 -0500

    Update streamlit_app.py
    header with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('The Title of the App')
 streamlit.header('This is a header')
 streamlit.text('This is text')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
