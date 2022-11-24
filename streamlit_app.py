e4fa11853872a0b5932e55986cec67aa5291408a
jaydlove <51937167+jaydlove@users.noreply.github.com>
2022-11-24 00:08:01 -0500

    Update streamlit_app.py
    updated to match example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.title('The Title of the App')
-streamlit.header('This is a header')
-streamlit.text('This is text')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
